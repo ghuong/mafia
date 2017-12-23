fe5ea4a5485d3538b9817959d50a57d9db8e8bd8
Gary Huong <garyhuong@gmail.com>
2017-12-23 16:49:07 -0500

    Write feature test: room creation

--- test/features/room_creation_test.rb
+++ test/features/room_creation_test.rb
@@ -5,9 +5,25 @@ class RoomCreationTest < ActionDispatch::IntegrationTest
     visit root_path
     click_on 'new-room'
     fill_in 'name', with: 'Richard'
-    click_on 'submit'
+
+    # Creates a new User and Room
+    assert_difference ['User.count', 'Room.count'], 1 do
+      click_on 'submit'
+    end
+
+    # Get Room Code from the URL
+    room_code = current_path.split('/')[2]
+    assert_equal 200, page.status_code
+    assert_equal edit_settings_path(room_code), current_path
+    assert page.has_content?(room_code)
+
     within('#users-list') do
       assert page.has_css?('li', text: 'Richard')
     end
+
+    # Rejoin room
+    visit room_path(room_code)
+    assert_equal 200, page.status_code
+    assert_equal edit_settings_path(room_code), current_path
   end
 end
--- test/integration/new_room_test.rb
+++ test/integration/new_room_test.rb
@@ -2,28 +2,31 @@ require 'test_helper'
 
 # Models a user trying to create a new room
 class NewRoomTest < ActionDispatch::IntegrationTest
-  test "can create new room" do
-    # Get new room page
-    get new_room_path
-    assert_template 'rooms/new'
-    # Submit form with name
-    assert_difference ['User.count', 'Room.count'], 1 do
-      post rooms_path,
-           params: { name: "James" }
-    end
-    room = assigns(:room)
-    user = assigns(:user)
-    assert is_authenticated?
-    # Should be redirected to the settings page
-    assert_redirected_to edit_settings_path(room.code)
-    follow_redirect!
-    assert_template 'settings/edit'
-    # Ensure page displays the room code
-    assert_select "p", "#{room.code}"
-    # Access room again, should still redirect to settings page
-    get room_path(room.code)
-    assert_redirected_to edit_settings_path(room.code)
-    follow_redirect!
-    assert_template 'settings/edit'
-  end
+
+  # This test has been replaced with tests/features/room_creation_test.rb
+
+  # test "can create new room" do
+  #   # Get new room page
+  #   get new_room_path
+  #   assert_template 'rooms/new'
+  #   # Submit form with name
+  #   assert_difference ['User.count', 'Room.count'], 1 do
+  #     post rooms_path,
+  #          params: { name: "James" }
+  #   end
+  #   room = assigns(:room)
+  #   user = assigns(:user)
+  #   assert is_authenticated?
+  #   # Should be redirected to the settings page
+  #   assert_redirected_to edit_settings_path(room.code)
+  #   follow_redirect!
+  #   assert_template 'settings/edit'
+  #   # Ensure page displays the room code
+  #   assert_select "p", "#{room.code}"
+  #   # Access room again, should still redirect to settings page
+  #   get room_path(room.code)
+  #   assert_redirected_to edit_settings_path(room.code)
+  #   follow_redirect!
+  #   assert_template 'settings/edit'
+  # end
 end
