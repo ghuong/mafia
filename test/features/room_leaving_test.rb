3bf94e05eafb0829392b6f1fac61f6aaf1198eee
Gary Huong <garyhuong@gmail.com>
2017-12-23 20:37:35 -0500

    Add test to leave room

---
+++ test/features/room_leaving_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class RoomLeavingTest < ActionDispatch::IntegrationTest
+  test "it allows guests to join room" do
+    room_code = ""
+    within_session :hosts_window do
+      room_code = create_room
+    end
+
+    guest_name = "John"
+    join_room(room_code, guest_name)
+    assert_difference "User.count", -1 do
+      page.driver.submit :delete, users_path(room_code), {}
+    end
+    assert_equal root_path, current_path
+
+    within_session :hosts_window do
+      refresh_page
+      assert_not page.has_css?('li', text: guest_name)
+    end
+  end
+end
